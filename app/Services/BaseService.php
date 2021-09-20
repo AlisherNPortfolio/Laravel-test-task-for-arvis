49061a0ad47b6d7f609e81b6b6151eebabc53ada
Nasrullayev Alisher <myemail9445@inbox.ru>
2021-09-20 15:18:02 +0500

    response structure changed

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -4,12 +4,12 @@ namespace App\Services;
 
 abstract class BaseService
 {
-    protected function successResponse($data)
+    protected function successResponse($data, $code = 200)
     {
         return response()->json([
             'success' => true,
             'data' => $data
-        ]);
+        ], $code);
     }
 
     protected function errorResponse($message = '', $code = 0, $details = null)
