fa9ab210e3edf6bd9dda55b683852e8b0f3e95a8
Nasrullayev Alisher <myemail9445@inbox.ru>
2021-09-20 14:29:41 +0500

    menu

---
+++ resources/js/src/components/layouts/MainMenu.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="navbar-nav">
+        <template v-for="(menu, idx) in menuItems">
+            <router-link
+            :key="idx"
+            :to="menu.link"
+            class="nav-item nav-link"
+            >{{ menu.name }}</router-link>
+        </template>
+    </div>
+</template>
+<script>
+export default {
+    name: 'main-menu',
+    data() {
+        return {
+            menuItems: [
+                { name: 'Home', link: '/' },
+                { name: 'Expenditures', link: '/expenditures' },
+                { name: 'Measures', link: '/measures' },
+                { name: 'Products', link: '/products' }
+            ]
+        }
+    }
+}
+</script>
