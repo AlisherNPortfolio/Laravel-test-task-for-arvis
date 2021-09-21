4c14367f141a631c53fa0eab6e5dc7d954b18966
Nasrullayev Alisher <myemail9445@inbox.ru>
2021-09-21 10:49:15 +0500

    vue env base url

--- resources/js/src/plugins/axios.js
+++ resources/js/src/plugins/axios.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios';
 
-const URL_BASE = process.env.VUE_APP_BASE_URL;
+const URL_BASE = process.env.MIX_BASE_URL;
 const instance = axios.create({
     baseURL: URL_BASE,
     headers: {
